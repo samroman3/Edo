d9388430bc93796a5db975f39c251d44382873bf
Samuel Roman <samroman3@gmail.com>
2023-12-07 21:46:54 -0500

    file reorder, adds apptheme colors

--- caloriecounter/Supporting Files/AppTheme.swift
+++ caloriecounter/Supporting Files/AppTheme.swift
@@ -14,10 +14,21 @@ struct AppTheme {
     static let carrot = Color(red: 255 / 255, green: 88 / 255, blue: 51 / 255)
     static let prunes = Color(red: 0, green: 0, blue: 0)
     static let lavender = Color(red: 135 / 255, green: 97 / 255, blue: 216 / 255)
+    
+    static let coral = Color(red: 255 / 255, green: 127 / 255, blue: 80 / 255)
+    static let teal = Color(red: 79 / 255, green: 216 / 255, blue: 194 / 255)
+    static let goldenrod = Color(red: 218 / 255, green: 165 / 255, blue: 32 / 255)
+    static let sageGreen = Color(red: 183 / 255, green: 195 / 255, blue: 176 / 255)
+    static let dustyRose = Color(red: 192 / 255, green: 132 / 255, blue: 151 / 255)
+    static let skyBlue = Color(red: 135 / 255, green: 206 / 255, blue: 235 / 255)
+    static let softPurple = Color(red: 190 / 255, green: 160 / 255, blue: 220 / 255)
+    static let peach = Color(red: 255 / 255, green: 229 / 255, blue: 180 / 255)
+    
     static let grayLight = Color(red: 208 / 255, green: 208 / 255, blue: 208 / 255)
     static let grayMiddle = Color(red: 140 / 255, green: 140 / 255, blue: 140 / 255)
     static let grayDark = Color(red: 94 / 255, green: 94 / 255, blue: 94 / 255)
     static let grayExtra = Color(red: 28 / 255, green: 28 / 255, blue: 28 / 255)
+    
 
     static let titleFont = Font.system(size: 28, weight: .bold, design: .default)
     static let bodyFont = Font.system(size: 17, weight: .regular, design: .default)
---
+++ caloriecounter/Views/Statistics/DailySummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DailySummaryView.swift
+//  caloriecounter
+//
+//  Created by Sam Roman on 12/7/23.
+//
+
+import SwiftUI
+
+struct DailySummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailySummaryView()
+}
