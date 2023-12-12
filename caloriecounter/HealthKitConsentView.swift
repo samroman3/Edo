241c0fdd30fcfd02a9edae15d8e552d9182e66c4
Samuel Roman <samroman3@gmail.com>
2023-12-12 00:31:11 -0500

    adds healthkit and privacy consent in onboardingviews

--- caloriecounter/HealthKitConsentView.swift
+++ caloriecounter/HealthKitConsentView.swift
@@ -6,13 +6,159 @@
 //
 
 import SwiftUI
+import HealthKit
 
-struct HealthKitConsentView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CaloricCalculationFormView: View {
+    @State private var age: String = ""
+    @State private var weight: String = ""
+    @State private var height: String = ""
+    @State private var sex: String = ""
+    @State private var activityLevel: String = ""
+    @State private var showHealthKitConsent = false
+    @State private var unitSystem: UnitSystem = .metric
+    @State private var feet: Int = 0
+    @State private var inches: Int = 0
+
+       enum UnitSystem: String, CaseIterable {
+           case metric = "Metric"
+           case imperial = "Imperial"
+       }
+
+       var body: some View {
+           NavigationView {
+               Form {
+                   Section(header: Text("Your Details").foregroundColor(AppTheme.lime)) {
+                       TextField("Age", text: $age)
+                           .keyboardType(.numberPad)
+
+                       Picker("Unit System", selection: $unitSystem) {
+                           ForEach(UnitSystem.allCases, id: \.self) { unit in
+                               Text(unit.rawValue).tag(unit)
+                           }
+                       }
+
+                       if unitSystem == .metric {
+                           TextField("Weight (kg)", text: $weight)
+                               .keyboardType(.decimalPad)
+                           TextField("Height (cm)", text: $height)
+                               .keyboardType(.decimalPad)
+                       } else {
+                           TextField("Weight (lb)", text: $weight)
+                               .keyboardType(.decimalPad)
+                           HStack {
+                               Picker("Feet", selection: $feet) {
+                                   ForEach(0..<10, id: \.self) { i in
+                                       Text("\(i) ft").tag(i)
+                                   }
+                               }
+                               Picker("Inches", selection: $inches) {
+                                   ForEach(0..<12, id: \.self) { i in
+                                       Text("\(i) in").tag(i)
+                                   }
+                               }
+                           }
+                       }
+
+                       Picker("Sex", selection: $sex) {
+                           Text("Male").tag("Male")
+                           Text("Female").tag("Female")
+                           Text("Other").tag("Other")
+                       }
+
+                       Picker("Activity Level", selection: $activityLevel) {
+                           Text("Sedentary").tag("Sedentary")
+                           Text("Lightly Active").tag("Lightly Active")
+                           Text("Moderately Active").tag("Moderately Active")
+                           Text("Very Active").tag("Very Active")
+                       }
+                   }
+
+                   Button(action: {
+                       // Submit form data logic here
+                   }) {
+                       Text("Calculate Caloric Needs")
+                           .foregroundColor(.white)
+                           .frame(maxWidth: .infinity)
+                           .padding()
+                           .background(AppTheme.carrot)
+                           .cornerRadius(10)
+                   }
+               }
+            .navigationBarTitle("Set Your Goals", displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {
+                showHealthKitConsent.toggle()
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(AppTheme.carrot)
+            })
+            .sheet(isPresented: $showHealthKitConsent) {
+                HealthKitConsentView(isPresented: $showHealthKitConsent)
+            }
+        }
     }
 }
 
-#Preview {
-    HealthKitConsentView()
+struct HealthKitConsentView: View {
+    @Binding var isPresented: Bool
+    let healthStore = HKHealthStore()
+
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("Health Data Access")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+
+                Text("Eazeat would like to use data from the Health app to personalize your experience. This includes your gender, date of birth, height, and weight.")
+                    .font(.body)
+                    .foregroundColor(.secondary)
+                    .padding()
+
+                Button(action: {
+                    requestHealthKitPermission()
+                }) {
+                    Text("Continue with HealthKit")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+
+                Button("No Thanks") {
+                    isPresented = false
+                }
+                .foregroundColor(.red)
+
+                Spacer()
+            }
+            .padding()
+            .navigationBarTitle("HealthKit Integration", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Close") {
+                isPresented = false
+            })
+        }
+    }
+
+    private func requestHealthKitPermission() {
+        let readTypes = Set([
+            HKObjectType.characteristicType(forIdentifier: .biologicalSex)!,
+            HKObjectType.characteristicType(forIdentifier: .dateOfBirth)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!
+        ])
+
+        // Check if HealthKit is available
+        if HKHealthStore.isHealthDataAvailable() {
+            // Request authorization
+            healthStore.requestAuthorization(toShare: nil, read: readTypes) { (success, error) in
+                if success {
+                    // Permissions granted, proceed with fetching HealthKit data
+                    // Fetch and process HealthKit data here
+                } else {
+                    // Handle errors or lack of permissions
+                }
+            }
+        }
+    }
 }
--- caloriecounter/OboardingView.swift
+++ caloriecounter/OboardingView.swift
@@ -9,24 +9,27 @@ import SwiftUI
 
 struct OnboardingView: View {
     @State private var showConsent = false
-    // Include additional state variables as needed for the onboarding process
-    
+    @State private var consentGiven = false
+
     var body: some View {
         VStack {
-            if !showConsent {
+            if !showConsent && !consentGiven {
                 WelcomeScreen(onGetStarted: {
                     withAnimation(.spring()) {
                         showConsent.toggle()
                     }
                 })
                 .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+            }
+            else if !showConsent && consentGiven {
+                CaloricCalculationFormView()
+                    .transition(.asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom)))
             } else {
-                UserConsentScreen(onConsentGiven: {
-                    // Handle consent given
-                }, onNavigateBack: {
-                    withAnimation(.spring()) {
+                ConsentView(consentGiven: $consentGiven, onConsentGiven: {
+                    withAnimation(.spring()){
                         showConsent.toggle()
                     }
+                    
                 })
                 .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
             }
@@ -34,13 +37,13 @@ struct OnboardingView: View {
     }
 }
 
+
 struct WelcomeScreen: View {
     var onGetStarted: () -> Void
     
     var body: some View {
         VStack(spacing: 20) {
-            // Replace with your own images and text
-            Image("WelcomeImage")
+            Image("Logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding()
@@ -68,31 +71,65 @@ struct WelcomeScreen: View {
         .shadow(color: .gray, radius: 10, x: 0, y: 10)
     }
 }
-
-struct UserConsentScreen: View {
+struct ConsentView: View {
+    @Binding var consentGiven: Bool
     var onConsentGiven: () -> Void
-    var onNavigateBack: () -> Void
-    
+
     var body: some View {
         VStack {
-            Text("User Consent")
-                .font(AppTheme.titleFont)
-                .foregroundColor(AppTheme.lime)
-            Text("Please give your consent to continue")
-                .font(AppTheme.bodyFont)
-                .foregroundColor(AppTheme.grayMiddle)
-                .padding()
-            Button("Consent and Continue", action: onConsentGiven)
-                .font(AppTheme.bodyFont)
-                .foregroundColor(.white)
-                .padding()
-                .background(AppTheme.lavender)
-                .cornerRadius(10)
-            Button("Back", action: onNavigateBack)
-                .font(AppTheme.captionFont)
-                .foregroundColor(AppTheme.grayMiddle)
+            ScrollView {
+                Text("User Consent")
+                    .font(AppTheme.titleFont)
+                    .foregroundColor(AppTheme.lime)
+                    .padding()
+
+                Text("We Respect Your Privacy")
+                    .font(AppTheme.bodyFont)
+                    .foregroundColor(AppTheme.grayMiddle)
+                    .padding(.bottom)
+
+                Text("""
+                    Eazeat is committed to protecting your privacy. We ask for your consent to collect and use your health and dietary data to personalize your experience and provide you with accurate nutritional insights.
+                    
+                    Data Collection:
+                    - We collect data related to your meals, nutritional intake, and physical activities.
+                    - With your permission, we will access your HealthKit data to enhance your meal tracking and goal setting.
+                    
+                    Data Use:
+                    - Your data helps us tailor your dietary goals and recommendations.
+                    - We use your data to provide you with progress reports and nutritional analysis.
+                    
+                    Data Sharing:
+                    - We do not share your personal data with third parties without your explicit consent.
+                    - Anonymized data may be used for improving the app and related services.
+
+                    You have the right to withdraw your consent at any time in the app settings.
+                    
+                    By giving consent, you agree to our Privacy Policy and Terms of Service.
+                    """)
+                    .font(AppTheme.bodyFont)
+                    .foregroundColor(AppTheme.grayLight)
+                    .padding()
+
+                HStack {
+                    Button("Decline", action: {
+                        consentGiven = false
+                    })
+                    .font(AppTheme.bodyFont)
+                    .foregroundColor(.red)
+                    .padding()
+                    
+                    Button("Consent", action: {
+                        consentGiven = true
+                        onConsentGiven()
+                    })
+                    .font(AppTheme.bodyFont)
+                    .foregroundColor(AppTheme.lavender)
+                    .padding()
+                }
+            }
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(AppTheme.grayExtra)
         .cornerRadius(20)
         .shadow(color: .gray, radius: 10, x: 0, y: 10)
